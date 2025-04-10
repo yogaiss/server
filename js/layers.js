628ad8e0f603c1c51abdb7ae96c2bb60eeffcad0
yogaiss <124781690+yogaiss@users.noreply.github.com>
2025-04-10 21:08:14 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,12 +1,43 @@
+// Sekolah
 fetch('data/sekolah.geojson')
   .then(res => res.json())
   .then(data => {
     L.geoJSON(data, {
-      onEachFeature: function (feature, layer) {
-        layer.bindPopup(formatPopup(feature));
+      onEachFeature: (feature, layer) => {
+        layer.bindPopup(formatSekolahPopup(feature));
       },
-      pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, { radius: 6, fillColor: "#3388ff", color: "#fff", weight: 1, fillOpacity: 0.8 });
+      pointToLayer: (feature, latlng) => {
+        return L.circleMarker(latlng, { radius: 6, fillColor: "blue", color: "#fff", weight: 1 });
+      }
+    }).addTo(map);
+  });
+
+// Puskesmas
+fetch('data/puskesmas.geojson')
+  .then(res => res.json())
+  .then(data => {
+    L.geoJSON(data, {
+      onEachFeature: (feature, layer) => {
+        layer.bindPopup(formatPuskesmasPopup(feature));
+      },
+      pointToLayer: (feature, latlng) => {
+        return L.circleMarker(latlng, { radius: 6, fillColor: "green", color: "#fff", weight: 1 });
+      }
+    }).addTo(map);
+  });
+
+// Desa (polygon)
+fetch('data/desa.geojson')
+  .then(res => res.json())
+  .then(data => {
+    L.geoJSON(data, {
+      onEachFeature: (feature, layer) => {
+        layer.bindPopup(`<strong>${feature.properties.nama_desa}</strong>`);
+      },
+      style: {
+        color: "#ff7800",
+        weight: 2,
+        fillOpacity: 0.2
       }
     }).addTo(map);
   });
