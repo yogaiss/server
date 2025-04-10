e321d595b630b74e4292f0df365baad9b4393710
yogaiss <124781690+yogaiss@users.noreply.github.com>
2025-04-10 21:10:49 +0700

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,17 @@
-function formatPopup(feature) {
+function formatSekolahPopup(feature) {
   const props = feature.properties;
   return `
     <strong>${props.nama_sekolah}</strong><br>
     NPSN: ${props.npsn}<br>
     Jenjang: ${props.jenjang}<br>
+    Alamat: ${props.alamat}
+  `;
+}
+
+function formatPuskesmasPopup(feature) {
+  const props = feature.properties;
+  return `
+    <strong>${props.nama}</strong><br>
     Alamat: ${props.alamat}
   `;
 }
