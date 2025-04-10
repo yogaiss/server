4a79da742d183163f671860961bae984f077ef93
yogaiss <124781690+yogaiss@users.noreply.github.com>
2025-04-10 21:13:45 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,62 +1,65 @@
-const sekolahLayer = L.layerGroup();
-const puskesmasLayer = L.layerGroup();
-const desaLayer = L.layerGroup();
-
-// Sekolah
-fetch('data/sekolah.geojson')
-  .then(res => res.json())
-  .then(data => {
-    L.geoJSON(data, {
-      onEachFeature: (feature, layer) => {
-        layer.bindPopup(formatSekolahPopup(feature));
-      },
-      pointToLayer: (feature, latlng) => {
-        return L.circleMarker(latlng, { radius: 6, fillColor: "blue", color: "#fff", weight: 1 });
-      }
-    }).addTo(sekolahLayer);
-  });
-
-// Puskesmas
-fetch('data/puskesmas.geojson')
-  .then(res => res.json())
-  .then(data => {
-    L.geoJSON(data, {
-      onEachFeature: (feature, layer) => {
-        layer.bindPopup(formatPuskesmasPopup(feature));
-      },
-      pointToLayer: (feature, latlng) => {
-        return L.circleMarker(latlng, { radius: 6, fillColor: "green", color: "#fff", weight: 1 });
-      }
-    }).addTo(puskesmasLayer);
-  });
-
-// Desa
-fetch('data/desa.geojson')
-  .then(res => res.json())
-  .then(data => {
-    L.geoJSON(data, {
-      onEachFeature: (feature, layer) => {
-        layer.bindPopup(`<strong>${feature.properties.nama_desa}</strong>`);
-      },
-      style: {
-        color: "#ff7800",
-        weight: 2,
-        fillOpacity: 0.2
-      }
-    }).addTo(desaLayer);
-  });
-
-// Tambahkan ke map
-sekolahLayer.addTo(map);
-puskesmasLayer.addTo(map);
-desaLayer.addTo(map);
-
-// Layer control
-const baseLayers = {};
-const overlays = {
-  "Sekolah": sekolahLayer,
-  "Puskesmas": puskesmasLayer,
-  "Desa": desaLayer
+const layers = {
+  sekolah: {
+    file: "data/sekolah.geojson",
+    layer: L.layerGroup(),
+    color: "blue",
+    popup: (props) => `
+      <strong>${props.nama_sekolah}</strong><br>
+      NPSN: ${props.npsn}<br>
+      Jenjang: ${props.jenjang}<br>
+      Alamat: ${props.alamat}`
+  },
+  puskesmas: {
+    file: "data/puskesmas.geojson",
+    layer: L.layerGroup(),
+    color: "green",
+    popup: (props) => `
+      <strong>${props.nama}</strong><br>
+      Alamat: ${props.alamat}`
+  },
+  desa: {
+    file: "data/desa.geojson",
+    layer: L.layerGroup(),
+    style: {
+      color: "#ff7800",
+      weight: 2,
+      fillOpacity: 0.2
+    },
+    popup: (props) => `<strong>${props.nama_desa}</strong>`
+  }
 };
 
-L.control.layers(baseLayers, overlays, { collapsed: false }).addTo(map);
+// Loop semua layer
+Object.entries(layers).forEach(([key, config]) => {
+  fetch(config.file)
+    .then(res => res.json())
+    .then(data => {
+      const geoJson = L.geoJSON(data, {
+        onEachFeature: (feature, layer) => {
+          layer.bindPopup(config.popup(feature.properties));
+        },
+        ...(config.color && {
+          pointToLayer: (feature, latlng) =>
+            L.circleMarker(latlng, {
+              radius: 6,
+              fillColor: config.color,
+              color: "#fff",
+              weight: 1,
+              fillOpacity: 0.8
+            })
+        }),
+        ...(config.style && { style: config.style })
+      });
+
+      geoJson.addTo(config.layer);
+    });
+
+  config.layer.addTo(map);
+});
+
+// Layer control
+const overlays = {};
+Object.keys(layers).forEach(name => {
+  overlays[name.charAt(0).toUpperCase() + name.slice(1)] = layers[name].layer;
+});
+L.control.layers({}, overlays).addTo(map);
